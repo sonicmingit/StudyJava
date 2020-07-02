b4572f5bd0fb7925d7fe1f2463ac8a2b13bcf084
sonic-pc <sonicming@gmail.com>
2020-07-02 23:56:21 +0800

    测试OP

--- Java8/src/main/java/sonic/java8/optional/QuickCheckDemo.java
+++ Java8/src/main/java/sonic/java8/optional/QuickCheckDemo.java
@@ -1,6 +1,10 @@
 package sonic.java8.optional;
 
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -16,8 +20,19 @@ public class QuickCheckDemo {
         t1.getDao1().getStrList().get(0);
     }
 
+
     private void testCheckOp(){
         TestDAO t1 = new TestDAO();
         Optional<String> s = Optional.ofNullable(t1).map(u -> u.getDao1()).map(u -> u.getStr()).map(String::toString);
+    }
+
+    @Test
+    public void testOp(){
+        TestDAO t = new TestDAO();
+        TestDAO1 t1 = new TestDAO1();
+        t.setDao1(t1);
+        t1.setStrList(new ArrayList<>());
+        Integer integer = Optional.ofNullable(t).map(TestDAO::getDao1).map(TestDAO1::getStrList).map(List::size).orElse(100);
+        System.out.println(integer);
     }
 }
